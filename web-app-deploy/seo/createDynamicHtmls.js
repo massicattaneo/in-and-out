82fd11179a131b48a576f36746f0975bc611809d
Massimiliano Cattaneo <massi.cattaneo.it@gmail.com>
2018-06-05 08:00:19 +0200

    seo adjustments

--- web-app-deploy/seo/createDynamicHtmls.js
+++ web-app-deploy/seo/createDynamicHtmls.js
@@ -7,7 +7,7 @@ function getTitle(string) {
 
 function map(sheets, posts) {
     const urls = [];
-    const description = 'In&amp;Out es tu centro de belleza en Málaga por excelencia, pioneros en la técnica de la depilación con hilo en Málaga y en tratamientos de belleza.'
+    const description = 'In&amp;Out es tu centro de belleza en Málaga por excelencia, pioneros en la técnica de la depilación con hilo en Málaga y en tratamientos de belleza.';
     const template = fs.readFileSync(__dirname + '/index.html', 'utf8');
     const homeHtml = template
         .replace('{{body}}', fs.readFileSync(__dirname + '/es/home.html', 'utf8'))
@@ -18,40 +18,55 @@ function map(sheets, posts) {
     urls.push({ url: '/', html: homeHtml });
     urls.push({ url: '/es', html: homeHtml });
 
-    function createHtmlList(appName) {
+    function createHtmlList(appName, subpath = () => '') {
         const list = sheets[appName];
         return `
         <div>
             <h1>${es.apps[appName].windowTitle}</h1>
             <ul>
                 ${list.map(function (item) {
-                    return `<li><a href="/es/${es.apps[appName].url}/${item.href}">${item.titulo}</a></li>`
-                }).join('')}
+            return `<li><a 
+            href="/es/${es.apps[appName].url}${subpath(item)}/${item.href}">${item.titulo}</a></li>`;
+        }).join('')}
             </ul>
         </div>
     `;
     }
 
+    const oldUrls = ['beauty-corner', 'beauty-parties', null,'contacto', null,'in-out-go', null, 'quienes-somos'];
     ['beautycorner', 'beautyparties', 'bookings', 'callUs', 'hours', 'inandoutgo', 'photoMeasure', 'team']
-        .forEach(function (name) {
+        .forEach(function (name, index) {
             const app = es.apps[name];
-            urls.push({
-                url: `/es/${app.url}`,
-                html: template
-                    .replace('{{body}}', fs.readFileSync(__dirname + `/es/${name}.html`, 'utf8'))
-                    .replace(/{{title}}/g, getTitle(app.windowTitle))
-                    .replace(/{{description}}/g, app.windowDescription)
-            });
+            const html = template
+                .replace('{{body}}', fs.readFileSync(__dirname + `/es/${name}.html`, 'utf8'))
+                .replace(/{{title}}/g, getTitle(app.windowTitle))
+                .replace(/{{description}}/g, app.windowDescription);
+            const url = `/es/${app.url}`;
+            urls.push({ url: url, html: html });
+            if (oldUrls[index])
+                urls.push({ url: `/oldUrls[index]/`, html: html });
+                urls.push({ url: `/oldUrls[index]`, html: html });
         });
 
     ['promotions', 'bonusCards', 'news', 'press'].forEach(function (name) {
+        const html = template
+            .replace('{{body}}', createHtmlList(name))
+            .replace(/{{title}}/g, getTitle(es.apps[name].windowTitle))
+            .replace(/{{description}}/g, es.apps[name].windowDescription);
         urls.push({
             url: `/es/${es.apps[name].url}`,
-            html: template
-                .replace('{{body}}', createHtmlList(name))
-                .replace(/{{title}}/g, getTitle(es.apps[name].windowTitle))
-                .replace(/{{description}}/g, es.apps[name].windowDescription)
+            html: html
         });
+        if (name === 'news') {
+            urls.push({
+                url: `/novedades/`,
+                html: html
+            });
+            urls.push({
+                url: `/novedades`,
+                html: html
+            });
+        }
         urls.push(...sheets[name].map(item => {
             return {
                 url: `/es/${es.apps[name].url}/${item.href}`,
@@ -67,12 +82,21 @@ function map(sheets, posts) {
     });
 
     ['treatments'].forEach(function (name) {
+        const html = template
+            .replace('{{body}}', createHtmlList(name, item => `/${item.tipo.toLowerCase()}`))
+            .replace(/{{title}}/g, getTitle(es.apps[name].windowTitle))
+            .replace(/{{description}}/g, es.apps[name].windowDescription);
         urls.push({
             url: `/es/${es.apps[name].url}`,
-            html: template
-                .replace('{{body}}', createHtmlList(name))
-                .replace(/{{title}}/g, getTitle(es.apps[name].windowTitle))
-                .replace(/{{description}}/g, es.apps[name].windowDescription)
+            html: html
+        });
+        urls.push({
+            url: `/tratamientos-de-belleza/`,
+            html: html
+        });
+        urls.push({
+            url: `/tratamientos-de-belleza`,
+            html: html
         });
         urls.push(...sheets[name].map(item => {
             return {
@@ -91,13 +115,12 @@ function map(sheets, posts) {
     urls.push({
         url: `/es/${es.apps.photos.url}`,
         html: template
-            .replace('{{body}}', 'LA FOTOS DE IN&OUT')
+            .replace('{{body}}', `<div>LA FOTOS DE IN&OUT</div>`)
             .replace(/{{title}}/g, getTitle(es.apps.photos.windowTitle))
             .replace(/{{description}}/g, es.apps.photos.windowDescription)
     });
 
     posts
-        .filter(p => p.post_mime_type === '')
         .filter(p => p.post_type === 'post')
         .forEach(function (post) {
             urls.push({
@@ -106,7 +129,7 @@ function map(sheets, posts) {
                     .replace('{{body}}', `<h1>${post.post_title}</h1><p>${post.post_content}</p>`)
                     .replace(/{{title}}/g, getTitle(post.post_title))
                     .replace(/{{description}}/g, getTitle(post.post_excerpt))
-            })
+            });
         });
 
     return urls;
