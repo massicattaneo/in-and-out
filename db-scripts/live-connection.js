1ba1e83d19d8f7844dd8c41f80f2007ee5d7701e
Massimiliano Cattaneo <massi.cattaneo.it@gmail.com>
2018-08-27 13:12:18 +0200

    case insensitive login services

--- db-scripts/live-connection.js
+++ db-scripts/live-connection.js
@@ -1,8 +1,8 @@
 const access = require('../web-app-deploy/private/mongo-db-access');
 const MongoClient = require('mongodb').MongoClient;
 const config = access.config;
-const ObjectID = require('mongodb').ObjectID;
-
+const ObjectId = require('mongodb').ObjectID;
+const fs = require('fs');
 const url = `mongodb://${config.mongo.user}:${encodeURIComponent(access.password)}@${config.mongo.hostString}`;
 
 MongoClient.connect(url, function (err, db) {
@@ -15,6 +15,31 @@ MongoClient.connect(url, function (err, db) {
     //         { $set : { 'email' : doc.email.toLowerCase() } }
     //     );
     //     console.log(doc.email)
+    // });
+
+    /** MERGE CLIENTS */
+    // const oldClient = '5b55e6c98556f5001f316cc6';
+    // const newClient = '5b7fe7f703c35a001fa3d399';
+    // db.collection('bonus').find({ clientId: ObjectId(oldClient) }).forEach(function (doc) {
+    //     db.collection('bonus').update(
+    //         { _id: doc._id },
+    //         { $set: { clientId: ObjectId(newClient) } }
+    //     );
+    //     console.log(doc);
+    // });
+    // db.collection('cash').find({ clientId: ObjectId(oldClient) }).forEach(function (doc) {
+    //     db.collection('cash').update(
+    //         { _id: doc._id },
+    //         { $set: { clientId: ObjectId(newClient) } }
+    //     );
+    //     console.log(doc);
+    // });
+
+    // /** CSV OF NO ACTIVE USERS */
+    // db.collection('users').find({ active: false }).toArray(function (err, users) {
+    //     if (err) return console.log('error');
+    //     fs.writeFileSync('./db-scripts/temp.csv', users.map(doc => `${doc.name},${doc.email},${doc.tel}`).join('\n'));
+    //     console.log('finish');
     // });
 
 });
