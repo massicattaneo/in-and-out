193c7783a0e7e265bc49bc02c285281e55f0fc45
mcattaneo <massi.cattaneo.it@gmail.com>
2018-09-27 18:47:30 +0200

    IE 11 - windows more appeal

--- modules/gml-polyfills/index.js
+++ modules/gml-polyfills/index.js
@@ -8,6 +8,22 @@ import './Function';
 import './HTMLElement';
 import {RetryRequest} from "../gml-http-request";
 
+(function () {
+
+    if ( typeof window.CustomEvent === "function" ) return false;
+
+    function CustomEvent ( event, params ) {
+        params = params || { bubbles: false, cancelable: false, detail: undefined };
+        var evt = document.createEvent( 'CustomEvent' );
+        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+        return evt;
+    }
+
+    CustomEvent.prototype = window.Event.prototype;
+
+    window.CustomEvent = CustomEvent;
+})();
+
 window.RetryRequest = RetryRequest;
 // const remove = HTMLElement.prototype.addEventListener
 //     .bind(document.getElementById('public'))
